45c4ee0feb5a41c10c7a61f79612516ab7645f6f
Mapendo Kelly <make48949@edu.ucl.dk>
2023-09-21 11:58:48 +0200

    Add project files.

---
+++ Server/ClientModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ClientModels
+    {
+        public string ProductId { get; set; }
+        public decimal MinPrice { get; set; }
+        public decimal SellPrice { get; set; }
+        public double AuctionLength { get; set; }
+
+        public double StartTime { get; set; }
+        public double EndTime { get; set; }
+
+    }
+}
